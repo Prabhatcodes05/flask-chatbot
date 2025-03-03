465ce405fe48a20d33fc5b59d88708acb371afed
Prabhatcodes05 <prabhatanand.pa@gmail.com>
2025-03-04 03:44:20 +0530

    Removed on demand model loading

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import os
 
 app = Flask(__name__)
 
-tokenizer = None
-model = None
+# Load model and tokenizer during application startup (TensorFlow)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
+model = TFAutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
+
+# Initialize chat history outside the function
 chat_history_ids = None
 
 @app.route("/")
@@ -20,21 +23,23 @@ def chat():
     return get_Chat_response(input)
 
 def get_Chat_response(text):
-    global tokenizer, model, chat_history_ids
+    global chat_history_ids
 
-    if tokenizer is None or model is None:
-        tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small") #removed from_tf=True
-        model = TFAutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small") #removed from_tf=True
-
+    # Let's chat for 1 line to reduce memory usage.
     for step in range(1):
+        # encode the new user input, add the eos_token and return a tensor in TensorFlow
         new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='tf')
 
+        # append the new user input tokens to the chat history
         if chat_history_ids is not None:
             bot_input_ids = tf.concat([chat_history_ids, new_user_input_ids], axis=-1)
         else:
             bot_input_ids = new_user_input_ids
 
+        # generate a response while limiting the total chat history to 1000 tokens,
         chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last output tokens from bot
         response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
         return response
 
